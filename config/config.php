060342018b83ff4d985fc1179e68da8bd12c0541
Andrew Caya <andrewscaya@yahoo.ca>
2018-12-09 03:31:08 -0500

    2018-12-09 AC: Updates the configuration files.

--- config/config.php
+++ config/config.php
@@ -1,19 +1,26 @@
 <?php
 
-$baseConfig['appName'] = 'LightMVC Framework';
+$baseConfig['appName'] = 'LightMVC Framework Skeleton Application';
 
-/*$baseConfig['doctrine']['DBAL']['CONN_NAME'] = [
-    'driver'   => 'pdo_mysql',
-    'host'     => '127.0.0.1',
-    'user'     => 'USERNAME',
-    'password' => 'PASSWORD',
-    'dbname'   => 'DBNAME',
-];*/
-
-/*$baseConfig['doctrine']['ORM']['CONN_NAME'] = [
-    'driver'   => 'pdo_mysql',
-    'host'     => '127.0.0.1',
-    'user'     => 'USERNAME',
-    'password' => 'PASSWORD',
-    'dbname'   => 'DBNAME',
-];*/
+$baseConfig['routes'] = [
+	0 => [
+		'GET',
+		'/index[/{action}]',
+		'index',
+	],
+	1 => [
+		'GET',
+		'/products[/{action}]',
+		'product',
+	],
+	2 => [
+		'GET',
+		'/products/{action}/[{id:[0-9]+}]',
+		'product',
+	],
+	3 => [
+		'POST',
+		'/products/{action}',
+		'product',
+	],
+];
