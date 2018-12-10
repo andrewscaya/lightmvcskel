35dd9b5c2efdec03595e278f60fc04745853b6c1
Andrew Caya <andrewscaya@yahoo.ca>
2018-12-10 00:47:48 -0500

    2018-12-10 AC: Updates the Skeleton Application's configuration files.

--- config/config.php
+++ config/config.php
@@ -2,6 +2,46 @@
 
 $baseConfig['appName'] = 'The LightMVC Framework Skeleton Application';
 
+$baseConfig['view'] = [
+	'urlbaseaddr' => $baseConfig['URLBASEADDR'],
+	'logo' => $baseConfig['URLBASEADDR'] . 'img/logo.png',
+	'favicon' => $baseConfig['URLBASEADDR'] . 'favicon.ico',
+	'appname' => $baseConfig['appName'],
+	'title' => "Skeleton Application",
+	'author' => 'Andrew Caya',
+	'description' => 'Small CRUD application',
+	'css' =>
+	[
+		$baseConfig['URLBASEADDR'] . 'css/bootstrap.min.css',
+		$baseConfig['URLBASEADDR'] . 'css/dashboard.css',
+		$baseConfig['URLBASEADDR'] . 'css/bootstrap.custom.css',
+		$baseConfig['URLBASEADDR'] . 'css/dashboard.css',
+
+	],
+	'js' =>
+	[
+		$baseConfig['URLBASEADDR'] . 'js/jquery.min.js',
+		$baseConfig['URLBASEADDR'] . 'js/bootstrap.min.js',
+
+	],
+	'jsscripts' =>
+	[
+		//"<script>\n\t\tfunction getPage(page) {\n\n\t\t\tvar url = page;\n\n\t\t\tjq( \"#pageBody\" ).load( url );\n\n\t\t}\n\t</script>\n",
+
+	],
+	'links' =>
+	[
+		'Home' => $baseConfig['URLBASEADDR'] . 'index',
+
+	],
+	'navMenu' =>
+	[
+		'Home' => $baseConfig['URLBASEADDR'] . 'index',
+
+	],
+
+];
+
 $baseConfig['routes'] = [
 	0 => [
 		'GET',
@@ -19,13 +59,18 @@ $baseConfig['routes'] = [
 		'product',
 	],
 	3 => [
+		'POST',
+		'/products[/{action}]',
+		'product',
+	],
+	4 => [
 		'GET',
 		'/products/{action}/[{id:[0-9]+}]',
 		'product',
 	],
-	4 => [
+	5 => [
 		'POST',
-		'/products/{action}',
+		'/products/{action}/[{id:[0-9]+}]',
 		'product',
 	],
 ];
