534cc5dc33867a8686706c2b31c17c7f3184c627
Andrew Caya <andrewscaya@yahoo.ca>
2018-12-31 15:37:00 -0500

    2018-12-31 AC: Modifies the C404Controller to implement the MiddlewareInterface.

--- controllers/Application/Controllers/C404Controller.php
+++ controllers/Application/Controllers/C404Controller.php
@@ -3,11 +3,16 @@
 namespace Application\Controllers;
 
 use Ascmvc\Mvc\Controller;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Zend\Diactoros\Response;
 
 
-class C404Controller extends Controller
+class C404Controller extends Controller implements MiddlewareInterface
 {
-	
+
     public function indexAction($vars = null)
     {
         $this->view['bodyjs'] = 1;
@@ -17,6 +22,14 @@ class C404Controller extends Controller
         $this->view['statuscode'] = 404;
         
         return $this->view;
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $response = new Response();
+        $response->getBody()->write('Kinda wrong! :)');
+        $response = $response->withStatus(404);
+        return $response;
     }
     
 }
