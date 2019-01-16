7034d4339659c6e545bf420f1f26ddca32883d12
Andrew Caya <andrewscaya@yahoo.ca>
2019-01-16 04:55:05 -0500

    2019-01-15 AC: Fixes a few templates.

--- templates/plates_tailwindcss/c404_index.php
+++ templates/plates_tailwindcss/c404_index.php
@@ -7,35 +7,36 @@
     <?=$this->section('head', $this->fetch('head', ['view' => $view]))?>
 <?php endif ?>
 
-  <body>
-  <?=$this->section('navbar', $this->fetch('navbar', ['view' => $view]))?>
+<body>
+<?=$this->section('navbar', $this->fetch('navbar', ['view' => $view]))?>
 
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-3 col-md-2 sidebar">
-          <ul class="nav nav-sidebar">
-          <?php foreach($view['navmenu'] as $key => $value): ?>
-              <li><?php echo '<a href="' . $value . '">' . $key . '</a>' ?></li>
-          <?php endforeach; ?>
-          </ul>
+<div id="pageBodyProducts">
+
+    <!-- hero -->
+    <div class="w-full py-24 px-6 bg-grey-lighter relative z-10">
+
+        <div class="container max-w-xl mx-auto text-left flex items-center flex-wrap">
+
+            <div class="w-full md:w-2/3">
+                <h1 class="text-2xl md:text-4xl text-grey-darkest mb-3">Oops! This ain't Kansas anymore!</h1>
+                <p class="text-md md:text-lg text-grey-dark leading-normal">
+                    Let's go back <a href="<?php echo $view['urlbaseaddr'] ?>index">HOME</a>!
+                </p>
+            </div>
         </div>
-        
-        <div id="pageBody">
-          <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-            <h1>Oops! This ain't Kansas anymore!</h1>
-            <p>Let's go back <a href="<?php echo $view['urlbaseaddr'] ?>index/index">HOME</a>!</p>
-          </div>
-        </div> <!-- END pageBody -->
-        
-      </div>
+
     </div>
+    <!-- /hero -->
+</div> <!-- END pageBody -->
 
-  <?php if ($view['bodyjs'] === 1): ?>
-      <?=$this->section('bodyjs', $this->fetch('bodyjs', ['view' => $view]))?>
-  <?php endif ?>
+<?=$this->section('footer', $this->fetch('footer', ['view' => $view]))?>
 
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="<?php echo $view['urlbaseaddr'] ?>js/ie10-viewport-bug-workaround.js"></script>
-    
-  </body>
+<?php if ($view['bodyjs'] === 1): ?>
+    <?=$this->section('bodyjs', $this->fetch('bodyjs', ['view' => $view]))?>
+<?php endif ?>
+
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="<?php echo $view['urlbaseaddr'] ?>js/ie10-viewport-bug-workaround.js"></script>
+
+</body>
 </html>
