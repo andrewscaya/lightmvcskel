384b490ab38ae6ac0850fd5a99f627170018da7e
Andrew Caya <andrewscaya@yahoo.ca>
2019-04-01 21:12:05 -0400

    2019-04-01 AC: Adds the new session configuration parameters.

--- config/session.config.php
+++ config/session.config.php
@@ -1,6 +1,9 @@
 <?php
 
 $baseConfig['session'] = [
+    'psr6_cache' => \Ascmvc\Cache\DoctrineCacheItemPool::class,
+    'doctrine_cache_driver' => \Doctrine\Common\Cache\FilesystemCache::class,
+    'doctrine_cache_params' => BASEDIR . DIRECTORY_SEPARATOR . 'cache' . DIRECTORY_SEPARATOR,
     'session_name' => 'PHPSESSION',
     'session_path' => '/',
     'session_id_length' => 32,
